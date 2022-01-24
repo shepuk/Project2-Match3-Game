8ced3e5cec00febe2704d9bf09476ca0f81b8556
shepuk <paulshepherd90@live.co.uk>
2022-01-24 19:48:22 +0000

    Began adding functionality to detect legal moves within the game

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 createBoard();
 
-
 tiles.forEach(tile => tile.addEventListener('dragstart', dragStart));
 tiles.forEach(tile => tile.addEventListener('drop', onDrop));
 
@@ -52,18 +51,53 @@ $('.tile').on('dragover',function(event){
     event.preventDefault();
 })
 
+/**
+ * logs the colour and id of a picked up tile
+ */
 function dragStart() {
     draggedTileColor = this.style.backgroundColor;
     idOfDraggedTile = parseInt(this.id);
-    console.log(draggedTileColor, this.id, 'picked up');
+    console.log(this.id, 'picked up');
 }
 
+/**
+ * logs the colour and id of the tile to be replaced
+ * switches the colours of the dragged and replaced tiles
+ */
 function onDrop() {
     replacedTileColor = this.style.backgroundColor;
     idOfReplacedTile = parseInt(this.id);
     this.style.backgroundColor = draggedTileColor;
     tiles[idOfDraggedTile].style.backgroundColor = replacedTileColor;
-    console.log(replacedTileColor, this.id, 'droped');
+    console.log(this.id, 'droped');
 }
 
+let adjacentTiles = [
+    idOfDraggedTile -playAreaWidth, // up (using playAreaWidth variable allows functionality to change board size instead of using intergers)
+    idOfDraggedTile +playAreaWidth, // down
+    idOfDraggedTile -1, //left
+    idOfDraggedTile +1 //right
+]
+
 })
+
+
+//TODO!
+
+//CURRENT DESIRED FEATURE LIST
+//Detect valid moves (up, down, left, right)
+//Allow movement only with valid moves
+//Detect tiles of same colour as a sucessful move
+//non-sucessful moves should revert tiles back
+//spawn game board with at least a handful of moveable tiles as to not game over immidiately
+//spawn game board without any sucessful moves at the start
+//animate feedback - notify players of moves and non-valid moves etc.
+//Animate tiles - both swapping tiles and hovering over tiles (possibly achieve this with jQuery)
+//menu to start game and show controls
+//contact page using email API
+
+//EXTRAS IF TIME ALLOWS
+//high scores
+//incorporate timer and / or score
+//images on tiles for a better overall look
+//allow player to change game board size
