38be4e5391dd8419ac5a8a2b742f6f029bba38f3
shepuk <paulshepherd90@live.co.uk>
2022-01-25 17:29:18 +0000

    Implemented legal and non-legal move checker. Colours will only change when a legal move is made

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const playAreaWidth = 9;
     const tiles = []; //array to hold created tile/div elements
 
-    const tileColors = [  //colours generated at coolors.co
+    const tileColors = [ //colours generated at coolors.co
         '#7B7554',
         '#17183B',
         '#A11692',
@@ -32,57 +32,76 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-createBoard();
+    createBoard();
 
-tiles.forEach(tile => tile.addEventListener('dragstart', dragStart));
-tiles.forEach(tile => tile.addEventListener('drop', onDrop));
+    tiles.forEach(tile => tile.addEventListener('dragstart', dragStart));
+    tiles.forEach(tile => tile.addEventListener('drop', onDrop));
+    tiles.forEach(tile => tile.addEventListener('dragend', dragEnd));
 
-let draggedTileColor; //variable to store the colour value of picked up tile
-let replacedTileColor; //variable to store the colour value of the tile which is replaced
-let idOfDraggedTile; //variable to store the id number of a picked up tile
-let idOfReplacedTile; //variable to store the id number of tile which is replaced
+    let draggedTileColor; //variable to store the colour value of picked up tile
+    let replacedTileColor; //variable to store the colour value of the tile which is replaced
+    let idOfDraggedTile; //variable to store the id number of a picked up tile
+    let idOfReplacedTile; //variable to store the id number of tile which is replaced
 
-//The two below preventDefault functions allow the drop event to be triggered in chrome
-//Fix from https://stackoverflow.com/questions/21339924/drop-event-not-firing-in-chrome
-$('.tile').on('dragenter',function(event){
-    event.preventDefault();
-})
-$('.tile').on('dragover',function(event){
-    event.preventDefault();
-})
+    //The two below preventDefault functions allow the drop event to be triggered in chrome
+    //Fix from https://stackoverflow.com/questions/21339924/drop-event-not-firing-in-chrome
+    $('.tile').on('dragenter', function (event) {
+        event.preventDefault();
+    })
+    $('.tile').on('dragover', function (event) {
+        event.preventDefault();
+    })
 
-/**
- * logs the colour and id of a picked up tile
- */
-function dragStart() {
-    draggedTileColor = this.style.backgroundColor;
-    idOfDraggedTile = parseInt(this.id);
-    console.log(this.id, 'picked up');
-}
+    /**
+     * logs the colour and id of a picked up tile
+     */
+    function dragStart() {
+        draggedTileColor = this.style.backgroundColor;
+        idOfDraggedTile = parseInt(this.id);
+        console.log(this.id, 'picked up');
+    }
 
-/**
- * logs the colour and id of the tile to be replaced
- * switches the colours of the dragged and replaced tiles
- */
-function onDrop() {
-    replacedTileColor = this.style.backgroundColor;
-    idOfReplacedTile = parseInt(this.id);
-    this.style.backgroundColor = draggedTileColor;
-    tiles[idOfDraggedTile].style.backgroundColor = replacedTileColor;
-    console.log(this.id, 'droped');
-}
+    /**
+     * logs the colour and id of the tile to be replaced
+     * switches the colours of the dragged and replaced tiles
+     */
+    function onDrop() {
+        replacedTileColor = this.style.backgroundColor;
+        idOfReplacedTile = parseInt(this.id);
+        this.style.backgroundColor = draggedTileColor;
+        tiles[idOfDraggedTile].style.backgroundColor = replacedTileColor;
+        console.log(this.id, 'droped');
+    }
 
-let adjacentTiles = [
-    idOfDraggedTile -playAreaWidth, // up (using playAreaWidth variable allows functionality to change board size instead of using intergers)
-    idOfDraggedTile +playAreaWidth, // down
-    idOfDraggedTile -1, //left
-    idOfDraggedTile +1 //right
-]
+    function dragEnd() {
+        let adjacentTiles = [
+            idOfDraggedTile - playAreaWidth, // up (using playAreaWidth variable allows functionality to change board size instead of using intergers)
+            idOfDraggedTile + playAreaWidth, // down
+            idOfDraggedTile - 1, //left
+            idOfDraggedTile + 1 //right
+        ]
+
+        let legalMove = adjacentTiles.includes(idOfReplacedTile); //if the id number of a replaced tile is included in the adjacenttiles array, then legalMove is truthy
+
+        if (idOfReplacedTile && legalMove) {
+            idOfReplacedTile = null;
+            console.log('legal move');
+        } else if (idOfReplacedTile && !legalMove) {
+            tiles[idOfReplacedTile].style.backgroundColor = replacedTileColor;
+            tiles[idOfDraggedTile].style.backgroundColor = draggedTileColor;
+            console.log('not a legal move');
+        } else tiles[idOfDraggedTile].style.backgroundColor = draggedTileColor, console.log('not a legal move');
+
+    }
+
+    
 
 })
 
 
 //TODO!
+
+//Bug found - colour change not working when switching tiles with first tile in the array (top left)
 
 //CURRENT DESIRED FEATURE LIST
 //Detect valid moves (up, down, left, right)
