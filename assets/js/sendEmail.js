e7a7bd3774db4f74a0ea8d35c8ec3de487510ba4
shepuk <paulshepherd90@live.co.uk>
2022-02-15 16:48:01 +0000

    Added functionality to clear form fields and display a message once email has be sent from the contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,23 +1,32 @@
+let contactName;
+let contactEmail;
+let enquiry;
+let submitMessage;
+
+contactName = document.getElementById('name');
+contactEmail = document.getElementById('email');
+enquiry = document.getElementById('enquiry');
+submitMessage = document.getElementById('form-submit-message');
+
+
 function sendMail(contactForm) {
     emailjs.send('gmail', 'CrateMatch Website', {
         'from_name': contactForm.name.value,
         'from_email': contactForm.email.value,
         'message': contactForm.enquiry.value
     })
-    .then(
-        function(response) {
-            console.log('Success', response);
-        },
-        function(error) {
-            console.log('Failed', error);
-        }
-    )
-    formReset();  // Reset all form data
-    return false;
-}
+        .then(
+            function (response) {
+                console.log('Success', response);
+                contactName.value = '';
+                contactEmail.value = '';
+                enquiry.value = '';
+                submitMessage.innerHTML = 'Thank you for your message';
+            },
+            function (error) {
+                console.log('Failed', error);
+            }
+        )
 
-function formReset(contactForm) {
-    contactForm.name.value = '';
-    contactForm.email.value = '';
-    contactForm.enquiry.value = '';
+    return false;
 }
