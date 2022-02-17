1ef6daa5b48c40d2467b1cef321368727e06c441
shepuk <paulshepherd90@live.co.uk>
2022-02-17 18:42:59 +0000

    Implemented a simple mobile navigation menu and updated footer for better responsiveness

---
+++ assets/js/mobileNav.js
@@ -0,0 +1,8 @@
+function openLinks() {
+  var links = document.getElementById("mob-nav-links");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+}
